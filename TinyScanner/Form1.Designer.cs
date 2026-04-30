dbdc451c44c519e8c34465044003becb06c783a6
Hassan@ <hasanalsal89@gmail.com>
2026-04-30 22:22:25 +0300

    Update Form1.Designer.cs

--- TinyScanner/Form1.Designer.cs
+++ TinyScanner/Form1.Designer.cs
@@ -16,10 +16,6 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             textBox1 = new TextBox();
@@ -29,18 +25,13 @@
             button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
-            // 
-            // textBox1
-            // 
+            
             textBox1.Location = new Point(27, 59);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(373, 344);
             textBox1.TabIndex = 0;
             textBox1.TextChanged += textBox1_TextChanged;
-            // 
-            // dataGridView1
-            // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2 });
             dataGridView1.Location = new Point(500, 59);
@@ -49,23 +40,14 @@
             dataGridView1.Size = new Size(366, 344);
             dataGridView1.TabIndex = 1;
             dataGridView1.CellContentClick += dataGridView1_CellContentClick;
-            // 
-            // Column1
-            // 
             Column1.HeaderText = "Type";
             Column1.MinimumWidth = 6;
             Column1.Name = "Column1";
             Column1.Width = 125;
-            // 
-            // Column2
-            // 
             Column2.HeaderText = "Token";
             Column2.MinimumWidth = 6;
             Column2.Name = "Column2";
             Column2.Width = 125;
-            // 
-            // button1
-            // 
             button1.BackColor = SystemColors.ActiveCaption;
             button1.Font = new Font("Algerian", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             button1.Location = new Point(372, 456);
@@ -75,9 +57,6 @@
             button1.Text = "Submit";
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
-            // 
-            // Form1
-            // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(906, 544);
